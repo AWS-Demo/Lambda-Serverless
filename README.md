8f510f084fee1cfcac1c71ecbc6cfc01f96975b4
Sri Harsha <Shree.Harsha@india.nec.com>
2018-04-01 11:18:30 +0530

    Creating project

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Lambda-Serverless
 
 AWS Lambda Serverless services integrated with Dynamo DB. Maven plugin to upload the code to AWS and create functions on push
+
+Maven command to push: clean install lambda:deploy-lambda
+
+Maven shade plugin is used to for attaching the dependencies to the output JAR during the build time
---
+++ src/main/java/com/ge/services/account/find/GetAccount.java
@@ -0,0 +1,30 @@
+package com.ge.services.account.find;
+
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.ge.services.account.model.Account;
+
+public class GetAccount implements RequestHandler<Account, Account> {
+
+	private DynamoDBMapper mapper;
+	
+	public GetAccount() {
+		AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+		client.setRegion(Region.getRegion(Regions.US_EAST_1));
+		mapper = new DynamoDBMapper(client);
+	}
+	
+	@Override
+	public Account handleRequest(Account account, Context ctx) {
+		LambdaLogger logger = ctx.getLogger();
+		logger.log("Account: " + account.getId());
+		account = mapper.load(Account.class, account.getId());
+		return account;
+	}
+
+}
